23c8e60728d98fd01b5059f594b96757c703d1de
jonat <jonat@LAPTOP-CFLOVVM0>
2019-04-22 16:04:34 -0300

    ConnectionFactory feita

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -1,6 +1,33 @@
 package dao;
 
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.gjt.mm.mysql.Driver;
+
+import java.sql.Connection;
+import java.sql.Connection;
+
+
+
 public class ConnectionFactory {
 	
-	DriverManager
+	public static final String URL = "jdbc:mysql://localhnost/ifsp_farmacia";
+	public static final String USER = "root";
+	public static final String PASS = "Bicicleta14";
+	
+	public static Connection getConnection()
+	{
+		try
+		{
+			DriverManager.registerDriver(new Driver());
+			return DriverManager.getConnection(URL, USER, PASS);
+		}
+		catch(SQLException ex)
+		{
+			ex.printStackTrace();
+			return null;
+		}
+	}
 }
