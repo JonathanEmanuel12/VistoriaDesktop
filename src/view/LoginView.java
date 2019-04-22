fd5f464a442382aa38112b4bb39fa9b48db0274d
jonat <jonat@LAPTOP-CFLOVVM0>
2019-04-22 16:29:10 -0300

    Criação da janela de cadastro

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -22,26 +22,37 @@ public class LoginView {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		janela = new JFrame("Login - Vistoria");
+		janela.setSize(400, 400);
 		panel = new JPanel();
 		janela.add(panel);
 		
 		lblNome = new JLabel("Nome: ");
 		panel.add(lblNome);
 		
-		txtNome = new JTextField();
+		txtNome = new JTextField(40);
 		panel.add(txtNome);
 		
 		lblLogin = new JLabel("Login: ");
 		panel.add(lblLogin);
 		
-		txtLogin = new JTextField();
+		txtLogin = new JTextField(40);
 		panel.add(txtLogin);
 		
 		lblSenha = new JLabel("Senha: ");
 		panel.add(lblSenha);
+
+		txtSenha = new JTextField(40);
+		panel.add(txtSenha);
+
 		
 		lblConfirmarSenha = new JLabel("Confirme sua senha: ");
 		panel.add(lblConfirmarSenha);
+		
+		txtConfirmarSenha = new JTextField(40);
+		panel.add(txtConfirmarSenha);
+		
+		janela.setVisible(true);
+		
 			}
 
 
