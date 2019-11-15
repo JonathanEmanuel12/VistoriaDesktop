9bfa5a8931deede872662051e8141d6805c37ac0
JonathanEmanuel12 <jonathaanemanuel21@gmail.com>
2019-11-15 17:56:04 -0300

    Classe de testes feitas: TerrenoControle e UsuarioControle

---
+++ testes-src/testesControle/testeUsuarioControle.java
@@ -0,0 +1,98 @@
+package testesControle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import controle.UsuarioControle;
+import dao.UsuarioDAO;
+import modelo.Usuario;
+
+class testeUsuarioControle {
+
+	Usuario usuario = new Usuario();
+	UsuarioControle controle = new UsuarioControle();
+	
+	@Test
+	void testCadastrarUsuarioLoginMenor() {
+		usuario.setLogin("mar");
+		usuario.setNome("Mariano");
+		usuario.setSenha("12345");
+		usuario.setTipo(1);
+		
+		Assertions.assertEquals("Login deve conter de 4 a 8 caracteres", controle.cadastrarUsuario(usuario));
+	}
+	
+	@Test
+	void testCadastrarUsuarioSenhaMenor() {
+		usuario.setLogin("mario");
+		usuario.setNome("Mariano");
+		usuario.setSenha("123");
+		usuario.setTipo(1);
+		
+		Assertions.assertEquals("Senha deve conter de 4 a 8 caracteres", controle.cadastrarUsuario(usuario));
+	}
+	
+	@Test
+	void testCadastrarUsuarioLoginMaior() {
+		usuario.setLogin("mariano123");
+		usuario.setNome("Mariano");
+		usuario.setSenha("12345");
+		usuario.setTipo(1);
+		
+		Assertions.assertEquals("Login deve conter de 4 a 8 caracteres", controle.cadastrarUsuario(usuario));
+	}
+	
+	@Test
+	void testCadastrarUsuarioSenhaMaior() {
+		usuario.setLogin("mario");
+		usuario.setNome("Mariano");
+		usuario.setSenha("1234567891");
+		usuario.setTipo(1);
+		
+		Assertions.assertEquals("Senha deve conter de 4 a 8 caracteres", controle.cadastrarUsuario(usuario));
+	}
+	
+	@Test
+	void testCadastrarUsuario() {
+		usuario.setLogin("mario");
+		usuario.setNome("Mariano");
+		usuario.setSenha("12345");
+		usuario.setTipo(1);
+		
+		Assertions.assertEquals("Usuario cadastrado com sucesso", controle.cadastrarUsuario(usuario));
+	}
+
+	@Test
+	void testBuscarUsuarioIncorreto() {
+		String login = "mario89";
+		String senha = "1234554";
+		Assertions.assertEquals(false, controle.buscarUsuario(login, senha));
+	}
+	
+	@Test
+	void testBuscarUsuarioCorreto() {
+		String login = "mario";
+		String senha = "12345";
+		Assertions.assertEquals(true, controle.buscarUsuario(login, senha));
+	}
+
+	@Test
+	void testBuscarEstagiariosLivres() {
+		UsuarioDAO dao = new UsuarioDAO();
+		List<Usuario> estagiarios = null;
+		
+		try {
+			estagiarios = dao.buscarEstagiariosLivres();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assertions.assertEquals(estagiarios, controle.buscarEstagiariosLivres());
+	}
+
+}
---
+++ testes-src/testesControle/testesTerrenoControle.java
@@ -0,0 +1,76 @@
+package testesControle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import controle.TerrenoControle;
+import dao.TerrenoDAO;
+import modelo.Terreno;
+
+class testesTerrenoControle {
+	
+	TerrenoControle controle = new TerrenoControle();
+	Terreno terreno = new Terreno();
+
+	@Test
+	void testCadastrarTerrenoLatitudeIncorreta() {
+		terreno.setLatitude("56°46´´8 ´L");
+		terreno.setLongitude("54°46´´55´S");
+		terreno.setEstado("Minas Gerais");
+		terreno.setCidade("Extrema");
+		terreno.setBairro("Roseira2");
+		terreno.setRua("Francisco Constantino");
+		terreno.setNumero(21);
+		
+		Assertions.assertEquals("Verifique se os campos Latitude e Longitude estão preenchidos", controle.cadastrarTerreno(terreno));
+	}
+	
+	@Test
+	void testCadastrarTerrenoEnderecoIncorreto() {
+		terreno.setLatitude("56°46´´88´L");
+		terreno.setLongitude("54°46´´55´S");
+		terreno.setEstado("Minas Gerais");
+		terreno.setCidade("");
+		terreno.setBairro("Roseira2");
+		terreno.setRua("Francisco Constantino");
+		terreno.setNumero(21);
+		
+		Assertions.assertEquals("Verifique se os campos do endereço estão preenchidos", controle.cadastrarTerreno(terreno));
+	}
+	
+	@Test
+	void testCadastrarTerreno() {
+		terreno.setLatitude("56°46´´88´L");
+		terreno.setLongitude("54°46´´55´S");
+		terreno.setEstado("Minas Gerais");
+		terreno.setCidade("Extrema");
+		terreno.setBairro("Roseira2");
+		terreno.setRua("Francisco Constantino");
+		terreno.setNumero(21);
+		
+		Assertions.assertEquals("Terreno adicionado com sucesso", controle.cadastrarTerreno(terreno));
+	}
+
+	@Test
+	void testBuscarTerrenosLivres() {
+		
+		TerrenoDAO dao = new TerrenoDAO();
+		List<Terreno> terrenos = null;
+		
+		try {
+			terrenos = dao.buscarTerrenosLivres();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Assertions.assertEquals(terrenos, controle.buscarTerrenosLivres());
+		
+	}
+
+}
